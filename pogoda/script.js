e479270214e475346457ebde37a09ee4a5cc4cf2
Filakiewicz.jr <oliwierfilak08@gmail.com>
2024-12-21 14:16:23 -0800

    Update script.js

--- pogoda/script.js
+++ pogoda/script.js
@@ -22,7 +22,7 @@ if (navigator.geolocation) {
             .then(response => response.json())
             .then(data => displayWeather(data))
             .catch(error => {
-                weatherElement.innerHTML = "Error fetching weather data.";
+                weatherElement.innerHTML = "Error fetching weather data. (Does not work on Opera GX idk why)";
                 console.error(error);
             });
     }, function(error) {
